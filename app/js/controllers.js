b12f64577a5546f0be8b8c3c55e020176a4033ce
Gael du Plessix <gael.duplessix@gmail.com>
2013-10-16 23:16:15 +0200

    Added Bootstrap

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,12 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('HomeController', [function() {
 
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('AboutController', [function() {
+
+  }])
+  .controller('ContactController', [function() {
 
   }]);
