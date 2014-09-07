a512c7f5a49d967e3e0a58b760c3f908346b6d61
Gael du Plessix <gael.duplessix@gmail.com>
2014-09-07 17:58:38 +0200

    Re-indented main controller

--- app/js/Controllers/MainController.js
+++ app/js/Controllers/MainController.js
@@ -1,36 +1,36 @@
 'use strict';
 
 angular.module('shace.controllers').controller('MainController',
-    ['$scope', '$rootScope', '$translate', '$sce', 'Shace', 'AccessToken',
-        function ($scope, $rootScope, $translate, $sce, Shace, AccessToken) {
+    ['$scope', '$rootScope', '$translate', '$sce', '$modal', 'Shace', 'AccessToken',
+        function ($scope, $rootScope, $translate, $sce, $modal, Shace, AccessToken) {
+            $scope.isHome = true;
+            $scope.currentState = undefined;
 
-    $scope.isHome = true;
-    $scope.currentState = undefined;
+            $scope.$on('$stateChangeSuccess', function (event, state) {
+                $scope.isHome = (state.controller === 'HomeController');
+                $scope.currentState = state;
+            });
 
-    $scope.$on('$stateChangeSuccess', function (event, state) {
-        $scope.isHome = (state.controller === 'HomeController');
-        $scope.currentState = state;
-    });
+            $scope.keyboardAction = function (event) {
+                $scope.$broadcast('keyboadAction', event);
+            };
 
-    $scope.keyboardAction = function (event) {
-        $scope.$broadcast('keyboadAction', event);
-    };
+            $scope.changeLanguage = function (lang) {
+                Shace.accessToken.lang = lang;
+                Shace.lang = lang;
+                Shace.storeAccessToken();
+                $translate.use(lang);
 
-    $scope.changeLanguage = function (lang) {
-        Shace.accessToken.lang = lang;
-        Shace.lang = lang;
-        Shace.storeAccessToken();
-        $translate.use(lang);
+                // Store lang server side
+                AccessToken.changeLanguage({'language':lang}, {});
+            }
 
-        // Store lang server side
-        AccessToken.changeLanguage({'language':lang}, {});
-    }
+            // Home video
+            $scope.homeVideoSrc = '';
+            $rootScope.$on('$translateChangeSuccess', function() {
+                $translate('HOME_VIDEO_URL').then(function(t) {
+                    $scope.homeVideoSrc = $sce.trustAsResourceUrl(t);
+                });
+            });
 
-    // Home video
-    $scope.homeVideoSrc = '';
-    $rootScope.$on('$translateChangeSuccess', function() {
-        $translate('HOME_VIDEO_URL').then(function(t) {
-            $scope.homeVideoSrc = $sce.trustAsResourceUrl(t);
-        });
-    });
-}]);
+        }]);
